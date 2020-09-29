ce24980234ef10e0b2aa41c3368290017287cae9
Dmitry Romashov <framebassman@gmail.com>
2020-09-29 09:55:13 +0000

    Deploy kolenkainc/homebrew-nanny to github.com/kolenkainc/homebrew-nanny.git:master

--- Formula/nanny.rb
+++ Formula/nanny.rb
@@ -1,8 +1,8 @@
 class Nanny < Formula
     desc "Nanny - is a command line interface for creation Github Pull Requests for projects with multiple release versions."
     homepage "https://github.com/kolenkainc/Nanny"
-    url "https://github.com/kolenkainc/Nanny/releases/download/1.0.0.172/nanny_1.0.0.172.tar.gz"
-    sha256 "f5159bc8db17de598423f499f75df61c837b83411f01887b6eeb395ab9b8d456"
+    url "https://github.com/kolenkainc/Nanny/releases/download/1.0.0.177/nanny_1.0.0.177.tar.gz"
+    sha256 "e6eaa334520bf111ac9057bc756921cafdcc9330bb35a0300fae0bae0199d7d8"
     license "MIT"
   
     livecheck do
